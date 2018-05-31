f001617c30e60e38da111be3601ec6e06f265bcd
OA Wu <comdan66@gmail.com>
2018-05-31 09:55:28 +0800

    Finish

--- OAGM.js
+++ OAGM.js
@@ -7,7 +7,7 @@
 
 function OAGM (option) {
   this._div = null;
-  this._option = Object.assign ({className: '', top: 0, left: 0, width: 32, height: 32, html: '', map: null, position: null }, option);
+  this._option = Object.assign ({className: '', top: 0, left: 0, width: 32, height: 32, html: '', map: null, position: null, css: {} }, option);
   this._option.map && this.setMap (this._option.map);
 }
 function initOAGM () {
@@ -38,6 +38,10 @@ function initOAGM () {
         this._div.style.height = this._option.height + 'px';
         this._div.innerHTML = this._option.html;
 
+        for (var k in this._option.css)
+          if (!(k == 'width' || k == 'height' || k == 'top' || k == 'left' || k == 'bottom' || k == 'right'))
+            this._div.style[k] = this._option.css[k];
+        
         var that = this;
         google.maps.event.addDomListener (this._div, 'click', function(e) { if (e.stopPropagation) e.stopPropagation (); google.maps.event.trigger (that, 'click'); });
 
@@ -57,35 +61,65 @@ function initOAGM () {
       return this;
     },
     setWidth: function (width) {
+      if (!this._div)
+        return this;
+
       this._option.width = width;
       this._div.style.width = this._option.width + 'px';
       this.setPoint ();
       return this;
     },
     setHeight: function (height) {
+      if (!this._div)
+        return this;
+
       this._option.height = height;
       this._div.style.height = this._option.height + 'px';
       this.setPoint ();
       return this;
     },
     setTop: function (top) {
+      if (!this._div)
+        return this;
+
       this._option.top = top;
       this._div.style.top = this._option.top + 'px';
       this.setPoint ();
       return this;
     },
     setLeft: function (left) {
+      if (!this._div)
+        return this;
+
       this._option.left = left;
       this._div.style.left = this._option.left + 'px';
       this.setPoint ();
       return this;
     },
     setHtml: function (html) {
+      if (!this._div)
+        return this;
+
       this._option.html = html;
       this._div.innerHTML = this._option.html;
       return this;
     },
+    setCss: function (css) {
+      if (!this._div)
+        return this;
+
+      this._option.css = css;
+
+      for (var k in this._option.css)
+        if (!(k == 'width' || k == 'height' || k == 'top' || k == 'left' || k == 'bottom' || k == 'right'))
+          this._div.style[k] = this._option.css[k];
+      
+      return this;
+    },
     setClassName: function (className) {
+      if (!this._div)
+        return this;
+
       this._option.className = className;
       this._div.className = this._option.className;
       return this;
